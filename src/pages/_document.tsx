b2dc96047dee453e63e39bf023704729a13fdddc
KoharaKazuya <reflect.results@gmail.com>
2020-09-27 12:00:48 +0900

    feat: CSP

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,25 @@
+import crypto from "crypto";
 import Document, { Head, Html, Main, NextScript } from "next/document";
 import React from "react";
 
+const cspHashOf = (text: string) => {
+  const hash = crypto.createHash("sha256");
+  hash.update(text);
+  return `'sha256-${hash.digest("base64")}'`;
+};
+
 export default class MyDocument extends Document {
   render() {
+    let csp = `default-src 'self'; script-src 'self' ${cspHashOf(
+      NextScript.getInlineScriptSource(this.props)
+    )}`;
+
+    if (process.env.NODE_ENV !== "production") {
+      csp = `style-src 'self' 'unsafe-inline'; font-src 'self' data:; default-src 'self'; script-src 'unsafe-eval' 'self' ${cspHashOf(
+        NextScript.getInlineScriptSource(this.props)
+      )}`;
+    }
+
     return (
       <Html lang="ja">
         <Head />
